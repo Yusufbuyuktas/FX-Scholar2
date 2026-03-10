0d38fbfc77668657f1c93ecfa1e0ba55648d9870
Faruk Turnalı <farukturnali11@gmail.com>
2026-03-10 12:26:26 +0300

    Course and Professor Interfaces implemented.

--- src/main/java/org/ahilmi/pro2_sm_2/service/CourseService.java
+++ src/main/java/org/ahilmi/pro2_sm_2/service/CourseService.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class CourseService {
+public class CourseService implements ICourseService{
 
     private final CourseRepository courseRepository;
 
--- src/main/java/org/ahilmi/pro2_sm_2/service/ProfessorService.java
+++ src/main/java/org/ahilmi/pro2_sm_2/service/ProfessorService.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class ProfessorService {
+public class ProfessorService implements IProfessorService{
 
     private final ProfessorRepository professorRepository;
 
