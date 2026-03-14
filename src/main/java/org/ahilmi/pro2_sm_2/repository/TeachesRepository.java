6a47646f551d9239e6c236b3541ace7f3cf7382e
ahilmii <ahmet1905hilmi@gmail.com>
2026-03-14 23:33:05 +0300

    teach exception implemented

--- src/main/java/org/ahilmi/pro2_sm_2/repository/TeachesRepository.java
+++ src/main/java/org/ahilmi/pro2_sm_2/repository/TeachesRepository.java
@@ -6,9 +6,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TeachesRepository extends JpaRepository<Teaches, Integer> {
-    
-    
 
+
+    boolean existsByProfessorIdAndCourseId(Integer professorId, Integer courseId);
 
 
 }
