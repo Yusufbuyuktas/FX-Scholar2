a3f94cd2b1a8fb23ea40ed9c5d6671daea15b398
ahilmii <ahmet1905hilmi@gmail.com>
2026-03-14 09:31:21 +0300

    exception structure added and course service and contoller updated

--- src/main/java/org/ahilmi/pro2_sm_2/service/CourseService.java
+++ src/main/java/org/ahilmi/pro2_sm_2/service/CourseService.java
@@ -2,6 +2,9 @@ package org.ahilmi.pro2_sm_2.service;
 
 import org.ahilmi.pro2_sm_2.dto.RequestCourseDTO;
 import org.ahilmi.pro2_sm_2.dto.ResponseCourseDTO;
+import org.ahilmi.pro2_sm_2.exception.ErrorMessages;
+import org.ahilmi.pro2_sm_2.exception.ResourceAlreadyExistsException;
+import org.ahilmi.pro2_sm_2.exception.ResourceNotFoundException;
 import org.ahilmi.pro2_sm_2.model.entity.Course;
 import org.ahilmi.pro2_sm_2.repository.CourseRepository;
 import org.springframework.beans.BeanUtils;
@@ -23,15 +26,22 @@ public class CourseService implements ICourseService{
     }
 
     public ResponseCourseDTO saveCourse(RequestCourseDTO requestCourseDTO) {
-     ResponseCourseDTO responseCourseDTO = new ResponseCourseDTO(); // response objesi oluşturldu çünkü en son cevap olark vereceğiz
-     Course course = new Course();// Db için çünkü dto ile değil entity ile çalışır
-     BeanUtils.copyProperties(requestCourseDTO, course); //DTO -> Entity dönüşümü
+        if (courseRepository.existsByName(requestCourseDTO.getName())) {
+            throw new ResourceAlreadyExistsException(ErrorMessages.ERROR_COURSE_ALREADY_EXIST);
+        }
+
+        ResponseCourseDTO responseCourseDTO = new ResponseCourseDTO(); // response objesi oluşturldu çünkü en son cevap olark vereceğiz
+        Course course = new Course();// Db için çünkü dto ile değil entity ile çalışır
+        BeanUtils.copyProperties(requestCourseDTO, course); //DTO -> Entity dönüşümü
         Course dbCourse = courseRepository.save(course);// Save method
         BeanUtils.copyProperties(dbCourse, responseCourseDTO); // // entity'deki verileri response DTO'ya aktarır
+
         return responseCourseDTO;
     }
+
+
     public List<ResponseCourseDTO> getAllCourses() {
-        List<ResponseCourseDTO> responseCourseDTOList = new ArrayList<>();
+         List<ResponseCourseDTO> responseCourseDTOList = new ArrayList<>();
          List<Course> courseList = courseRepository.findAll(); // SELECT * FROM course; gibi
          for (Course course : courseList) {
              ResponseCourseDTO responseCourseDTO = new ResponseCourseDTO(); //Veriyi kullanıcıya DTO formatında döndürcez
@@ -40,6 +50,7 @@ public class CourseService implements ICourseService{
          }
          return responseCourseDTOList;
     }
+
     public ResponseCourseDTO getCourseById(Integer id) {
         ResponseCourseDTO responseCourseDTO = new ResponseCourseDTO();
         Optional<Course> course = courseRepository.findById(id);
@@ -49,21 +60,20 @@ public class CourseService implements ICourseService{
             return responseCourseDTO;
         }
 
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND,
-                "Verilen ID ile eşleşen bir course bulunamadı: " + id);
+        throw new ResourceNotFoundException(ErrorMessages.ERROR_COURSE_NOT_FOUND);
 
     }
+
     public void deleteCourseById(Integer id) {
         Optional<Course> course = courseRepository.findById(id);// checking if there's a course
         if (course.isPresent()) {
-
             courseRepository.delete(course.get());
             return;
         }
 
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND,
-                "Silinecek course bulunamadı: " + id);
+        throw new ResourceNotFoundException(ErrorMessages.ERROR_COURSE_NOT_FOUND);
     }
+
     public ResponseCourseDTO updateCourseById(Integer id, RequestCourseDTO requestCourseDTO) {
         ResponseCourseDTO responseCourseDTO = new ResponseCourseDTO();
         Optional<Course> course = courseRepository.findById(id);
@@ -71,19 +81,13 @@ public class CourseService implements ICourseService{
             Course dbCourse = course.get();
             dbCourse.setName(requestCourseDTO.getName());
             dbCourse.setCredit(requestCourseDTO.getCredit());
+
             Course updatedCourse = courseRepository.save(dbCourse);
             BeanUtils.copyProperties(updatedCourse, responseCourseDTO);
             return responseCourseDTO;
-
-
         }
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Verilen ID ile eşleşen bir profesör bulunamadı: " + id);
-
+        throw new ResourceNotFoundException(ErrorMessages.ERROR_COURSE_NOT_FOUND);
     }
-
-
-
-
 
 
 }
