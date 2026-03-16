8839168b2e6062554d73a3ad6d220d4b37ea4a1f
ahilmii <ahmet1905hilmi@gmail.com>
2026-03-16 22:55:59 +0300

    validation errors handled

--- src/main/java/org/ahilmi/pro2_sm_2/exception/GlobalExceptionHandler.java
+++ src/main/java/org/ahilmi/pro2_sm_2/exception/GlobalExceptionHandler.java
@@ -2,9 +2,14 @@ package org.ahilmi.pro2_sm_2.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -29,5 +34,22 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage()));
     }
+
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class) // dto validasyon hataları için
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        // spring'in yakaladığı tüm validasyon hatalarını (name null, department null) döngüye al
+        ex.getBindingResult().getAllErrors().forEach((error) -> { // object error hatanın genel hali, bu yüzden aşağıda fielderrora çeviriyoruz
+            String fieldName = ((FieldError) error).getField(); // hatanın hangi alanda olduğunu öğrenmek için (name, credit, department)
+            String errorMessage = error.getDefaultMessage(); // dto sınıflarında yazdığımız "name cannot be null" gibi mesajları buarda alıyruz
+            errors.put(fieldName, errorMessage);
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
 
 }
